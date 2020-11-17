16951022e63c08f724ce0e10563fc1db070457f0
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-11-18 00:19:07 +0200

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,5 @@
 class SessionsController < ApplicationController
-  def new
-    user = User.find_by_username(params[:username])
-  end
+  def new; end
 
   def create
     user = User.find_by_username(params[:username])
