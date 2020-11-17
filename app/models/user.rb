7ec02dba747489b89cb507293eaa151791fc526b
Yanick <tyankee15@gmail.com>
2020-11-17 14:01:23 -0300

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :attendances, foreign_key: :attendee_id
   has_many :attended_events, through: :attendances
-  # <%= link_to 'Not Attending', attendance_path(attendee.attendances.find_by_attended_event_id(@event.id)), method: :delete%>
 end
